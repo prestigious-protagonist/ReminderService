db8a1e6813ba3c3dd74d8b8fd5bfde726ba5e90a
prestigious-protagonist <jaskaranyt123@gmail.com>
2025-01-04 21:37:27 +0530

    Setup of smtp using nodemailer done

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
 module.exports = {
-    PORT : process.env.PORT
+    PORT : process.env.PORT,
+    EMAIL_ID: process.env.EMAIL_ID,
+    EMAIL_PASS: process.env.EMAIL_PASS
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/server-config');
+const { sendBasicEmail } = require('./services/email-services');
 const startServer = () => {
     const app = express();
     app.listen(PORT, () => {
+        //from to subject body
+        sendBasicEmail('jaskaranyt123@gmail.com', 'singh.jaskaran2024@gmail.com', 'Testing', 'application is under testing phase');
         console.log(`Server Listening on port : ${PORT}`)
     })
 }
